c2b25539c4df0cc7f3c0d2748ab4ecfce4e6d20a
JieonLee <dlwldjs7544@naver.com>
2025-05-14 17:15:50 +0900

    feat : initialize project

---
+++ src/main/java/com/sparta/intern/backend/SpringBootJwtAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.intern.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/intern/backend/SpringBootJwtAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.intern.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
